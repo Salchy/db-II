4a5a6dbb9d64911c5a7ae8919dd0832b5c70db84
Salchy <leandrocg524@gmail.com>
2025-04-24 18:46:03 -0300

    Creacion de DB

---
+++ TP01/CreacionDB.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE BDArchivos
+GO
+
+CREATE TABLE TiposUsuario (
+	IDTipoUsuario INT PRIMARY KEY IDENTITY(1, 1),
+	TipoUsuario VARCHAR(100) NOT NULL
+)
+GO
+
+CREATE TABLE TiposArchivos (
+	IDTipoArchivo INT PRIMARY KEY IDENTITY(1, 1),
+	TipoArchivo VARCHAR(100) NOT NULL
+)
+GO
+
+CREATE TABLE Permisos (
+	IDPermiso INT PRIMARY KEY IDENTITY(1, 1),
+	Nombre VARCHAR(100) NOT NULL
+)
+GO
+
+CREATE TABLE Usuarios (
+	IDUsuario INT PRIMARY KEY IDENTITY(1, 1),
+	Nombre VARCHAR(100) NOT NULL,
+	Apellido VARCHAR(100) NOT NULL,
+	IDTipoUsuario INT NOT NULL,
+
+	FOREIGN KEY (IDTipoUsuario) REFERENCES TiposUsuario(IDTipoUsuario)
+)
+GO
+
+CREATE TABLE Archivos (
+	IDArchivo INT PRIMARY KEY IDENTITY(1, 1),
+	IDUsuarioDueño INT NOT NULL,
+	Nombre VARCHAR(255) NOT NULL,
+	Extension VARCHAR(10) NOT NULL,
+	Descripcion VARCHAR(1000) NULL,
+	IDTipoArchivo INT NOT NULL,
+	Tamaño INT NOT NULL,
+	FechaCreacion DATETIME NOT NULL DEFAULT GETDATE(),
+	FechaUltimaModificacion DATETIME NOT NULL DEFAULT GETDATE(),
+	Eliminado BIT NOT NULL DEFAULT 0,
+
+	FOREIGN KEY (IDUsuarioDueño) REFERENCES Usuarios(IDUsuario),
+	FOREIGN KEY (IDTipoArchivo) REFERENCES TiposArchivos(IDTipoArchivo)
+)
+GO
+
+CREATE TABLE ArchivosCompartidos (
+	IDArchivo INT NOT NULL,
+	IDUsuario INT NOT NULL,
+	IDPermiso INT NOT NULL,
+	FechaCompartido DATETIME NOT NULL DEFAULT GETDATE(),
+
+	PRIMARY KEY (IDArchivo, IDUsuario),
+	FOREIGN KEY (IDArchivo) REFERENCES Archivos(IDArchivo),
+	FOREIGN KEY (IDUsuario) REFERENCES Usuarios(IDUsuario),
+	FOREIGN KEY (IDPermiso) REFERENCES Permisos(IDPermiso)
+);
