acec3900fff757dddac38c92d3868bdf1a25e1ed
Salchy <leandrocg524@gmail.com>
2025-05-02 00:01:36 -0300

    Ejercicios 11 y 12

--- TP05/ActividadesTP5.sql
+++ TP05/ActividadesTP5.sql
@@ -18,18 +18,18 @@ WHERE Extension = 'pdf';
 -- Ejercicio 4 - La cantidad de extensiones distintas cuyos archivos tienen en su nombre o en su descripción la palabra 'Informe' o 'Documento'.
 SELECT COUNT(DISTINCT Extension) AS 'Cantidad extensiones'
 FROM Archivos
-WHERE Nombre LIKE '%informe%' OR Descripcion LIKE '%Documento%'
+WHERE Nombre LIKE '%informe%' OR Descripcion LIKE '%Documento%';
 
 -- Ejercicio 5 - El promedio de tamaño (expresado en Megabytes) de los archivos con extensión 'doc', 'docx', 'xls', 'xlsx'.
 SELECT ROUND(AVG(Tamaño) / 1024.0 / 1024.0, 4) AS 'Promedio (MB)'
 FROM Archivos
-WHERE Extension IN ('doc', 'docx', 'xls', 'xlsx')
+WHERE Extension IN ('doc', 'docx', 'xls', 'xlsx');
 
 -- Ejercicio 6 - La cantidad de archivos que le fueron compartidos al usuario con apellido 'Clarck'
 SELECT COUNT(*) AS 'Cantidad Comp. a Clark'
 FROM ArchivosCompartidos AS AC
 INNER JOIN Usuarios AS U ON AC.IDUsuario = U.IDUsuario
-WHERE U.Apellido = 'Clarck'
+WHERE U.Apellido = 'Clarck';
 
 -- Ejercicio 7 - La cantidad de tipos de usuario que tienen asociado usuarios que registren, como dueños, archivos con extensión pdf.
 SELECT COUNT(DISTINCT TU.TipoUsuario)
@@ -41,7 +41,7 @@ WHERE A.IDUsuarioDueño = U.IDUsuario AND A.Extension = 'pdf';
 -- Ejercicio 8 - El tamaño máximo expresado en Megabytes de los archivos que hayan sido creados en el año 2024.
 SELECT MAX(Tamaño) / 1024.0 / 1024.0 AS 'Tamaño maximo (MB)'
 FROM Archivos
-WHERE YEAR(FechaCreacion) = 2024
+WHERE YEAR(FechaCreacion) = 2024;
 
 -- Ejercicio 9 - El nombre del tipo de usuario y la cantidad de usuarios distintos de dicho tipo que registran, como
 -- dueños, archivos con extensión pdf.
@@ -63,15 +63,31 @@ FROM Archivos AS A
 INNER JOIN Usuarios AS U ON A.IDUsuarioDueño = U.IDUsuario
 INNER JOIN ArchivosCompartidos AS AC ON A.IDArchivo = AC.IDArchivo
 GROUP BY U.Nombre, U.Apellido
+ORDER BY SUM(A.Tamaño) DESC;
 
 -- Ejercicio 11 - El nombre del tipo de archivo y el promedio de tamaño de los archivos que corresponden a dicho
 -- tipo de archivo.
+SELECT TA.TipoArchivo, ROUND((AVG(A.Tamaño) * 1.0) / 1024.0 / 1024.0, 4) AS 'Promedio de tamaño (MB)'
+FROM Archivos AS A
+INNER JOIN TiposArchivos AS TA ON A.IDTipoArchivo = TA.IDTipoArchivo
+GROUP BY TA.TipoArchivo;
 
+-- EXTRA: Alternativa para mostrar el promedio de todos los tipos de archivos:
+-- Variación del JOIN con TiposArchivos
+SELECT TA.TipoArchivo, ISNULL(ROUND((AVG(A.Tamaño) * 1.0) / 1024.0 / 1024.0, 4), 0) AS 'Promedio de tamaño (MB)'
+FROM Archivos AS A
+RIGHT JOIN TiposArchivos AS TA ON A.IDTipoArchivo = TA.IDTipoArchivo
+GROUP BY TA.TipoArchivo;
 
--- Ejercicio 12 - Por cada extensión, indicar la extensión, la cantidad de archivos con esa extensión y el total acumulado en bytes. Ordenado por cantidad de archivos de forma ascendente.
+-- Ejercicio 12 - Por cada extensión, indicar la extensión, la cantidad de archivos con esa extensión y el total
+-- acumulado en bytes. Ordenado por cantidad de archivos de forma ascendente.
+SELECT A.Extension, COUNT(*) AS 'Cantidad de archivos', SUM(A.Tamaño) AS 'Acumulado (bytes)'
+FROM Archivos AS A
+GROUP BY A.Extension
+ORDER BY COUNT(*) ASC;
 
-
--- Ejercicio 13 - Por cada usuario, indicar IDUSuario, Apellido, Nombre y la sumatoria total en bytes de los archivos que es dueño. Si algún usuario no registra archivos indicar 0 en la sumatoria total.
+-- Ejercicio 13 - Por cada usuario, indicar IDUSuario, Apellido, Nombre y la sumatoria total en bytes de los
+-- archivos que es dueño. Si algún usuario no registra archivos indicar 0 en la sumatoria total.
 
 
 -- Ejercicio 14 - Los tipos de archivos que fueron compartidos más de una vez con el permiso con nombre 'Lectura'
