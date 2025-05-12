e1db30332df43802c2e0ea516bdc2bd7a760de41
Salchy <leandrocg524@gmail.com>
2025-05-12 14:17:54 -0300

    Funciones de usuario 2

--- vistas/funciones de usuario.sql
+++ vistas/funciones de usuario.sql
@@ -15,3 +15,26 @@ BEGIN
 	-- Resto de lógica, pueden ir IF, etc.
 	RETURN @resultado;
 END
+
+SELECT dbo.Calcular(2, 5) AS 'Resultado Suma';
+
+CREATE FUNCTION dbo.CalcularAños(@fecha1 DATE, @fecha2 DATE)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @años INT;
+    
+	-- Calcular la diferencia en años entre las dos fechas
+	SET @años = DATEDIFF(YEAR, @fecha1, @fecha2);
+
+	-- Restar un año si la persona aún no ha cumplido años en el año actual
+    IF (MONTH(@fecha2) < MONTH(@fecha1))
+        OR (MONTH(@fecha2) = MONTH(@fecha1) AND DAY(@fecha2) < DAY(@fecha1))
+    BEGIN
+        SET @años = @años - 1;
+    END
+
+	RETURN @años;
+END;
+
+SELECT dbo.CalcularAños('2000-06-27', GETDATE()) AS 'Edad';
